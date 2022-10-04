ed927d935e89a8fa6ac32965b9dd27490f6fd288
Arturo <arturo@sigma-telecom.com>
2022-10-04 17:09:39 -0300

    +uart: add a management struct

--- PA_UHF/Core/Inc/uart1.h
+++ PA_UHF/Core/Inc/uart1.h
@@ -10,16 +10,27 @@
 
 #include "stm32g0xx_hal.h"
 #include "stdbool.h"
+#include "stdlib.h"
 
-extern bool rxdata;
-extern uint32_t rxfne;
+#define  RX_BUFFLEN 25
+#define TX_BUFFLEN  100
 
-void uart1_init(uint32_t , uint32_t );
+typedef struct UART1 {
+	uint8_t *rx_buffer;
+	uint8_t *tx_buffer;
+	uint8_t rx_count;
+	bool is_data_ready;
+} UART1_t;
+
+void uart1_init(uint32_t, uint32_t, UART1_t*);
 void uart1_write(volatile char);
-void uart1_send_str(volatile char *);
-void uart1_send_frame(char*, uint8_t );
-void uart1_read(char *,uint8_t);
-void uart1_read_test(char *,uint8_t);
-char uart1_1byte_read(void);
+void uart1_send_str( char*);
+void uart1_send_frame(char*, uint8_t);
+void uart1_read(char*, uint8_t);
+void uart1_read_test(char*, uint8_t);
+uint8_t uart1_1byte_read(void);
+void uart1_read_to_frame(UART1_t *u);
+void uart1_clean_buffer(UART1_t*);
+uint8_t uart1_nonblocking_read(void);
 
 #endif /* INC_UART1_H_ */
--- PA_UHF/Core/Src/uart1.c
+++ PA_UHF/Core/Src/uart1.c
@@ -46,17 +46,17 @@ void uart1_gpio_init() {
 	CLEAR_BIT(GPIOA->AFR[1], GPIO_AFRH_AFSEL10_3);
 }
 
-void uart1_init(uint32_t pclk, uint32_t baud_rate) {
+void uart1_init(uint32_t pclk, uint32_t baud_rate, UART1_t *u) {
 	uint32_t br_value = 0;
 
 	uart1_gpio_init();
 
 	/*enable clock access to USART1 */
 	SET_BIT(RCC->APBENR2, RCC_APBENR2_USART1EN);
-	if(pclk == 16000000){
-	/*set HSI 16 CLK */
-	CLEAR_BIT(RCC->CCIPR,RCC_CCIPR_USART1SEL_0);
-	SET_BIT(RCC->CCIPR,RCC_CCIPR_USART1SEL_1);
+	if (pclk == 16000000) {
+		/*set HSI 16 CLK */
+		CLEAR_BIT(RCC->CCIPR, RCC_CCIPR_USART1SEL_0);
+		SET_BIT(RCC->CCIPR, RCC_CCIPR_USART1SEL_1);
 	}
 	//MODIFY_REG(USART1->PRESC,USART_PRESC_PRESCALER,0x0010);
 	/* set baud rate */
@@ -65,16 +65,23 @@ void uart1_init(uint32_t pclk, uint32_t baud_rate) {
 	/* transmitter enable*/
 	USART1->CR1 = USART_CR1_TE | USART_CR1_RE;
 
+	u->rx_buffer = (uint8_t*) malloc(RX_BUFFLEN);
+	u->tx_buffer = (uint8_t*) malloc(TX_BUFFLEN);
+	u->is_data_ready = false;
+	u->rx_count = 0;
+
+	uart1_clean_buffer(u);
+
 	/* enable FIFO */
-	//SET_BIT(USART1->CR2, USART_CR1_FIFOEN);
+	SET_BIT(USART1->CR2, USART_CR1_FIFOEN);
 	/* enable Rx timeout */
 	//SET_BIT(USART1->CR2, USART_CR2_RTOEN);
 	/**/
 	//MODIFY_REG(USART1->RTOR,USART_RTOR_RTO,100);
 	/*set length */
-	/* select */
-	//SET_BIT(USART1->CR1, USART_CR1_RXNEIE_RXFNEIE);
-	//NVIC_EnableIRQ(USART1_IRQn);
+	/* Enable interrupt */
+	SET_BIT(USART1->CR1, USART_CR1_RXNEIE_RXFNEIE);
+	NVIC_EnableIRQ(USART1_IRQn);
 	//uart1_dma_init();
 //
 	SET_BIT(USART1->CR1, USART_CR1_UE);
@@ -106,8 +113,9 @@ void uart1_write(char ch) {
 void uart1_read(char *data, uint8_t size) {
 	bool override = READ_BIT(USART1->ISR, USART_ISR_ORE);
 	bool data_present = READ_BIT(USART1->ISR, USART_ISR_RXNE_RXFNE);
-	bool busy = READ_BIT(USART1->ISR, USART_ISR_BUSY);
+//	bool busy = READ_BIT(USART1->ISR, USART_ISR_BUSY);
 	bool timeout = false;
+	uint16_t rxfne = 0;
 	if (size > 1) {
 		for (int i = 0; (i < size - 1); i++) {
 			while ((!data_present && !override && !timeout)) {
@@ -128,10 +136,10 @@ void uart1_read(char *data, uint8_t size) {
 	}
 }
 
-char uart1_1byte_read(void) {
+uint8_t uart1_1byte_read(void) {
 	bool override = READ_BIT(USART1->ISR, USART_ISR_ORE);
 	bool data_present = READ_BIT(USART1->ISR, USART_ISR_RXNE_RXFNE);
-	bool busy = READ_BIT(USART1->ISR, USART_ISR_BUSY);
+//	bool busy = READ_BIT(USART1->ISR, USART_ISR_BUSY);
 	if ((data_present || override)) {
 		if (override)
 			SET_BIT(USART1->ICR, USART_ICR_ORECF);
@@ -140,15 +148,64 @@ char uart1_1byte_read(void) {
 		return '\0';
 }
 
-void uart1_send_str(volatile char *str) {
+void  uart1_read_to_frame(UART1_t *u) {
+	if (u->rx_count >= RX_BUFFLEN) {
+		uart1_clean_buffer(u);
+		u->rx_count = 0;
+	}
+	u->rx_buffer[u->rx_count++] = uart1_1byte_read();
+}
+
+void uart1_send_str(char *str) {
 	uint8_t i;
 	for (i = 0; str[i] != '\0'; i++)
 		uart1_write(str[i]);
 }
 
 void uart1_send_frame(char *str, uint8_t len) {
+
 	if (len > 0) {
 		for (int i = 0; i < len; i++)
 			uart1_write(str[i]);
+	}
+}
+
+void uart1_clean_buffer(UART1_t *u) {
+	u->rx_count = 0;
+	if (TX_BUFFLEN > RX_BUFFLEN) {
+		for (int i = 0; i < TX_BUFFLEN; i++) {
+			if (i < RX_BUFFLEN)
+				u->rx_buffer[i] = 0x00;
+			u->tx_buffer[i] = 0x00;
+		}
+	} else {
+		for (int i = 0; i < RX_BUFFLEN; i++) {
+			if (i < TX_BUFFLEN)
+				u->tx_buffer[i] = 0x00;
+			u->rx_buffer[i] = 0x00;
+		}
+	}
+}
+
+uint8_t uart1_nonblocking_read(void) {
+	uint8_t rcvcount = 0;
+	uint32_t tickstart = HAL_GetTick();
+	bool timeout = false;
+	uint8_t timeout_value = 10;
+
+	while (rcvcount < RX_BUFFLEN && timeout == false) {
+		if (((HAL_GetTick() - tickstart) > timeout_value)
+				|| (timeout_value == 0U)) {
+			timeout = true;
+			if (READ_BIT(USART1->ISR, USART_ISR_ORE))
+				SET_BIT(USART1->ICR, USART_ICR_ORECF);
+			if (READ_BIT(USART1->ISR, USART_ISR_IDLE))
+				SET_BIT(USART1->ICR, USART_ICR_IDLECF);
+			if (READ_BIT(USART1->ISR, USART_ISR_FE))
+				SET_BIT(USART1->ICR, USART_ICR_FECF);
+		}
+
+		if (READ_BIT(USART1->ISR, USART_ISR_RXNE_RXFNE))
+			return USART1->RDR;
 	}
 }
