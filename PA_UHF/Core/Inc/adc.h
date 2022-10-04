e6b1da79daceda9004655434fe1bf7c59f8299af
Arturo <arturo@sigma-telecom.com>
2022-10-04 17:08:27 -0300

    empty file

---
+++ PA_UHF/Core/Inc/adc.h
@@ -0,0 +1,13 @@
+/*
+ * adc.h
+ *
+ *  Created on: Sep 29, 2022
+ *      Author: sigmadev
+ */
+
+#ifndef INC_ADC_H_
+#define INC_ADC_H_
+
+
+
+#endif /* INC_ADC_H_ */
---
+++ PA_UHF/Core/Src/adc.c
@@ -0,0 +1,8 @@
+/*
+ * adc.c
+ *
+ *  Created on: Sep 29, 2022
+ *      Author: sigmadev
+ */
+
+
