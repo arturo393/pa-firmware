a6d32b1eac1747008cab05e3aac08565f319a574
Arturo <arturo@sigma-telecom.com>
2022-10-27 17:07:41 -0300

    lm75 temperature driver

---
+++ PA_UHF/Core/Inc/lm75.h
@@ -0,0 +1,34 @@
+/* USER CODE BEGIN Header */
+/**
+ ******************************************************************************
+ * @file           : eeprom.h
+ * @brief          : Header for eeprom.c file.
+ *                   This file contains the common defines of the application.
+ ******************************************************************************
+ *
+ *
+ ******************************************************************************
+ */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __LM75_H
+#define __LM75_H
+
+
+#include "main.h"
+#include "i2c1.h"
+
+//  LM75B IIC address
+#define    LM75_ADDR 0x4f
+
+//  LM75B registers
+typedef enum LM75_REG {
+	LM75_Temp, LM75_Conf, LM75_Thyst, LM75_Tos
+} LM75_REG_t;
+
+void lm75_init();
+float lm75_read(void) ;
+
+
+#endif /* __LM75_H */
---
+++ PA_UHF/Core/Src/lm75.c
@@ -0,0 +1,19 @@
+#include "lm75.h"
+
+void lm75_init(void ) {
+	uint8_t cmd[2];
+	cmd[0] = LM75_Conf;
+	cmd[1] = 0x0;
+	i2c1_byteTransmit(LM75_ADDR<<1, cmd, 2);
+}
+
+float lm75_read(void) {
+	uint8_t cmd[2];
+	float result = 0;
+	cmd[0] = LM75_Temp;
+
+	i2c1_byteTransmit( LM75_ADDR<<1, cmd, 1); // Send command string
+	i2c1_buffReceive(LM75_ADDR<<1 | 1, cmd, 2); // Send command string
+	result = (float) ((cmd[0] << 8) | cmd[1]) / 256.0f;
+	return result;
+}
