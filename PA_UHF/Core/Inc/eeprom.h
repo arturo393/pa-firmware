149be6e43927190aab2f00c892ed305ad5667160
Arturo <arturo@sigma-telecom.com>
2022-10-27 17:09:53 -0300

    add page eeprom reading

--- PA_UHF/Core/Inc/eeprom.h
+++ PA_UHF/Core/Inc/eeprom.h
@@ -1,14 +1,14 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : eeprom.h
-  * @brief          : Header for eeprom.c file.
-  *                   This file contains the common defines of the application.
-  ******************************************************************************
-  *
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : eeprom.h
+ * @brief          : Header for eeprom.c file.
+ *                   This file contains the common defines of the application.
+ ******************************************************************************
+ *
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 
 /* Define to prevent recursive inclusion -------------------------------------*/
@@ -19,10 +19,8 @@
 extern "C" {
 #endif
 
-
 #include "main.h"
 #include "i2c1.h"
-
 
 uint8_t EEPROM_Read(uint8_t address);
 void EEPROM_Write(uint8_t address, uint8_t data);
@@ -33,25 +31,24 @@ void eeprom_write(uint8_t addr, uint16_t data);
 uint16_t eeprom_read(uint8_t address);
 uint8_t eeprom_1byte_read(uint8_t eaddress);
 void eeprom_1byte_write(uint8_t address, uint8_t data);
-void eeprom_test_write(uint8_t caddress,uint8_t address, uint8_t data);
-uint8_t eeprom_test_read(uint8_t caddress,uint8_t eaddress);
+void eeprom_test_write(uint8_t caddress, uint8_t address, uint8_t data);
+uint8_t eeprom_test_read(uint8_t caddress, uint8_t eaddress);
 
-#define EEPROM_CHIP_ADDR 0x4f
+#define EEPROM_CHIP_ADDR 0x50
 #define EEPROM_PAGE_SIZE 8
 #define EEPROM_PAGE_NUM 32
 
-typedef enum EEPROM_ADDR {
-	ATT_VALUE_ADDR,
-	POUT_ADC_MAX_ADDR,
-	POUT_ADC_MIN_ADDR,
-	POUT_IS_CALIBRATED_ADDR,
-	PIN_ADC_MAX_ADDR,
-	PIN_ADC_MIN_ADDR,
-	PIN_IS_CALIBRATED_ADDR,
-	VSWR_ADC_MAX_ADDR,
-	VSWR_ADC_MIN_ADDR,
-	VSWR_IS_CALIBRATED_ADDR,
-} EEPROM_ADDR;
+#define  BASE_ADDR 0x05
+#define 	ATT_VALUE_ADDR BASE_ADDR  + 8
+#define 	POUT_ADC_MAX_ADDR  BASE_ADDR + 16
+#define 	POUT_ADC_MIN_ADDR  BASE_ADDR + 32
+#define 	POUT_IS_CALIBRATED_ADDR BASE_ADDR + 40
+#define 	PIN_ADC_MAX_ADDR  BASE_ADDR + 48
+#define 	PIN_ADC_MIN_ADDR  BASE_ADDR + 56
+#define 	PIN_IS_CALIBRATED_ADDR  BASE_ADDR + 64
+#define 	VSWR_ADC_MAX_ADDR  BASE_ADDR + 72
+#define 	VSWR_ADC_MIN_ADDR  BASE_ADDR + 80
+#define 	VSWR_IS_CALIBRATED_ADDR  BASE_ADDR + 88
 
 #ifdef __cplusplus
 }
--- PA_UHF/Core/Src/eeprom.c
+++ PA_UHF/Core/Src/eeprom.c
@@ -1,7 +1,9 @@
 #include "eeprom.h"
 
 //extern I2C_HandleTypeDef hi2c1;
+
 /*
+ *
 uint8_t EEPROM_Read(uint8_t address) {
 	uint8_t buff[2];
 	buff[0] = address;
@@ -37,45 +39,55 @@ uint16_t EEPROM_2byte_Read(uint8_t address) {
 	return data;
 }
 */
+
 uint8_t eeprom_1byte_read(uint8_t addr) {
-	char data;
-	i2c1_byteTransmit(EEPROM_CHIP_ADDR << 1, &addr,1);
-	data = i2c1_byteReceive(EEPROM_CHIP_ADDR << 1 | 1,1);
+	uint8_t data;
+	i2c1_byteTransmit(EEPROM_CHIP_ADDR<<1 , &addr,1);
+	data = i2c1_byteReceive(EEPROM_CHIP_ADDR <<1 | 1,1);
 	return data;
 }
 
 void eeprom_1byte_write(uint8_t addr, uint8_t data) {
-	char  buff[2];
+	uint8_t  buff[2];
 	uint8_t stored_data;
 	buff[0] = addr;
 	buff[1] = data;
 
 	stored_data = eeprom_1byte_read(addr);
 	if (stored_data != data)
-		i2c1_byteTransmit(EEPROM_CHIP_ADDR << 1, buff,2);
+		i2c1_byteTransmit((char ) EEPROM_CHIP_ADDR<<1 , buff,2);
 }
 
 void eeprom_write(uint8_t addr, uint16_t data) {
 
-	char  buff[3];
-	uint8_t stored_data;
+	uint8_t  buff[1] = {0x00};
+	uint16_t stored_data;
 	buff[0] = addr;
 	buff[1] =  data >> 8;
 	buff[2] =  data & 0xff;
 
+
 	stored_data = eeprom_read(addr);
 	if (stored_data != data)
-		i2c1_byteTransmit(EEPROM_CHIP_ADDR << 1, buff,3);
+		i2c1_byteTransmit((char ) EEPROM_CHIP_ADDR << 1, buff,3);
 }
 
 uint16_t eeprom_read(uint8_t address) {
 	uint16_t data = 0;
-	char  buff[2];
-	i2c1_byteTransmit(EEPROM_CHIP_ADDR << 1, &address,1);
-	i2c1_buffReceive(EEPROM_CHIP_ADDR << 1 | 1,buff, 2);
-	data = buff[1]<< 8;
-	data |= buff[0];
+	uint8_t  buff[2] ={0};
 
+	i2c1_byteTransmit((char )EEPROM_CHIP_ADDR << 1, &address,1);
+	i2c1_buffReceive((char) EEPROM_CHIP_ADDR << 1 | 1,buff, 2);
+	data = buff[0]<< 8;
+	data |= buff[1];
+return data;
+}
+
+void eeprom_page_read(uint8_t address,uint8_t*data) {
+
+	// TODO revisar acá
+	i2c1_byteTransmit((char )EEPROM_CHIP_ADDR << 1, &address,1);
+	i2c1_buffReceive((char) EEPROM_CHIP_ADDR << 1 | 1,data, 256);
 }
 
 
