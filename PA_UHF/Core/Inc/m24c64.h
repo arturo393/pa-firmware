cfc7f33a990798f4b3a5cca8742de074c10def40
Arturo <arturo@sigma-telecom.com>
2022-11-03 17:09:10 -0300

    reduce memory ussage

--- PA_UHF/Core/Inc/m24c64.h
+++ PA_UHF/Core/Inc/m24c64.h
@@ -22,10 +22,13 @@ extern "C" {
 
 #include "main.h"
 #include "i2c1.h"
+#include "string.h"
 
 #define CHIP_ADDR 0xa0
 #define PAGE_SIZE 32
 #define PAGE_NUM 256
+#define IS_READY 0xaa
+#define PADDRPOSITION 5
 
 
 // EEPROM ADDRESS (8bits)
@@ -36,21 +39,24 @@ extern "C" {
 
 typedef enum{
  	ATT_VALUE_ADDR = BASE_ADDR ,
+	POUT_MAX_READY_ADDR,
  	POUT_ADC_MAX_ADDR_0 ,
 	POUT_ADC_MAX_ADDR_1,
+	POUT_MIN_READY_ADDR,
 	POUT_ADC_MIN_ADDR_0,
 	POUT_ADC_MIN_ADDR_1,
+ 	PIN_MAX_READY_ADDR,
  	PIN_ADC_MAX_ADDR_0 ,
  	PIN_ADC_MAX_ADDR_1,
+	PIN_MIN_READY_ADDR,
  	PIN_ADC_MIN_ADDR_0 ,
  	PIN_ADC_MIN_ADDR_1,
- 	VSWR_ADC_MAX_ADDR_0 ,
+	VSWR_MAX_READY_ADDR,
+	VSWR_ADC_MAX_ADDR_0 ,
  	VSWR_ADC_MAX_ADDR_1,
+	VSWR_MIN_READY_ADDR,
  	VSWR_ADC_MIN_ADDR_0,
- 	VSWR_ADC_MIN_ADDR_1 ,
- 	POUT_IS_CALIBRATED_ADDR,
- 	PIN_IS_CALIBRATED_ADDR,
- 	VSWR_IS_CALIBRATED_ADDR,
+ 	VSWR_ADC_MIN_ADDR_1
 }M24C64_ADDR_t;
 
 
@@ -62,6 +68,8 @@ typedef struct M24C64 {
 void m24c64_page_read(uint8_t address,uint8_t page, uint8_t *data);
 void m24c64_read_N(uint8_t page, uint8_t *data, uint8_t offset,uint8_t size);
 void m24c64_write_N(uint8_t page, uint8_t *data, uint8_t offset,uint8_t size);
+void m24c64_init_16uvalue(M24C64_ADDR_t addr,uint16_t value);
+void m24c64_store_16uvalue(M24C64_ADDR_t addr,uint16_t value);
 
 
 
--- PA_UHF/Core/Src/m24c64.c
+++ PA_UHF/Core/Src/m24c64.c
@@ -40,53 +40,62 @@
  }
  */
 
-
-void m24c64_page_read(uint8_t address,uint8_t page, uint8_t *data) {
-	// TODO revisar acá
+void m24c64_page_read(uint8_t address, uint8_t page, uint8_t *data) {
 	uint8_t buff[2] = { 0 };
-	uint8_t paddrposition = (log(PAGE_SIZE)/log(2));
-	uint16_t MemAddress = page<<paddrposition;
+	uint16_t MemAddress = page << PADDRPOSITION;
 
-	buff[0] = MemAddress >> 8 ;
+	buff[0] = MemAddress >> 8;
 	buff[1] = MemAddress & 0xff;
 
 	i2c1_byte_tx(CHIP_ADDR, buff, 2);
 	i2c1_buffReceive(CHIP_ADDR, data, 32);
 }
 
-void m24c64_read_N(uint8_t page, uint8_t *data, uint8_t offset,uint8_t size) {
-	// TODO revisar acá
+void m24c64_read_N(uint8_t page, uint8_t *data, uint8_t offset, uint8_t size) {
 	uint8_t buff[2] = { 0 };
-	uint8_t paddrposition = (log(PAGE_SIZE)/log(2));
-	uint16_t MemAddress = page<<paddrposition | offset;
+	uint16_t MemAddress = page << PADDRPOSITION | offset;
 
-	buff[0] = MemAddress >> 8 ;
+	buff[0] = MemAddress >> 8;
 	buff[1] = MemAddress & 0xff;
 
 	i2c1_byte_tx(CHIP_ADDR, buff, 2);
 	i2c1_buffReceive(CHIP_ADDR, data, size);
 }
 
-void m24c64_write_N(uint8_t page, uint8_t *data, uint8_t offset,uint8_t size) {
-	// TODO revisar acá
-	uint8_t buff[size+2];
-	uint8_t paddrposition = (log(PAGE_SIZE)/log(2));
-	uint16_t MemAddress = page<<paddrposition | offset;
+void m24c64_write_N(uint8_t page, uint8_t *data, uint8_t offset, uint8_t size) {
+	uint8_t buff[size + 2];
 	uint8_t read[size];
-	uint8_t tmp[size+2];
 
-	m24c64_read_N(page,read,offset,size);
+	m24c64_read_N(page, read, offset, size);
 
-	HAL_Delay(5);
-	if (strncmp(data,read,size)){
-		buff[0] = MemAddress >> 8 ;
-		buff[1] = MemAddress & 0xff;
-		for (int i = 0; i<size ; i++){
-			buff[i+2] = data[i];
+//	HAL_Delay(5);
+	if (strncmp((const char*) data, (const char*) read, (size_t) size)) {
+		buff[0] = (page << PADDRPOSITION | offset) >> 8;
+		buff[1] = (page << PADDRPOSITION | offset) & 0xff;
+		for (int i = 0; i < size; i++) {
+			buff[i + 2] = data[i];
 		}
-		i2c1_byte_tx(CHIP_ADDR , buff, size+2);
+		i2c1_byte_tx(CHIP_ADDR, buff, size + 2);
 	}
 	HAL_Delay(6);
 }
 
+void m24c64_init_16uvalue(M24C64_ADDR_t addr, uint16_t value) {
+	uint8_t buff[2];
+	m24c64_read_N(BASE_ADDR, buff, addr, 1);
+	if (!(buff[0] == IS_READY)) {
+		buff[0] = value >> 8;
+		buff[1] = value & 0xff;
+		m24c64_write_N(BASE_ADDR, buff, addr + 1, 2);
+	}
+}
+
+void m24c64_store_16uvalue(M24C64_ADDR_t addr, uint16_t value) {
+	uint8_t buff[2];
+	buff[0] = value >> 8;
+	buff[1] = value & 0xff;
+	m24c64_write_N(BASE_ADDR, buff, addr + 1, 2);
+	buff[0] = addr;
+	m24c64_write_N(BASE_ADDR, buff, addr, 1);
+}
 
