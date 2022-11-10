1349211c0478bf8c4133838e4b07a59f84592373
Arturo <arturo@sigma-telecom.com>
2022-11-10 14:24:43 -0300

    delay for temperature i2c reading

--- PA_UHF/Core/Src/lm75.c
+++ PA_UHF/Core/Src/lm75.c
@@ -13,6 +13,7 @@ float lm75_read(void) {
 	cmd[0] = LM75_Temp;
 
 	i2c1_byte_tx( LM75_ADDR<<1, cmd, 1); // Send command string
+	HAL_Delay(1);
 	i2c1_buffReceive(LM75_ADDR<<1 | 1, cmd, 2); // Send command string
 	result = (float) ((cmd[0] << 8) | cmd[1]) / 256.0f;
 	return result;
