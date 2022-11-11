f35d03b1529b8fbb311e9a81bd6b17f5b5217a36
Arturo <arturo@sigma-telecom.com>
2022-11-11 10:13:54 -0300

    remove unused function

--- PA_UHF/Core/Src/m24c64.c
+++ PA_UHF/Core/Src/m24c64.c
@@ -1,45 +1,5 @@
 #include <m24c64.h>
 
-//extern I2C_HandleTypeDef hi2c1;
-
-/*
- *
- uint8_t EEPROM_Read(uint8_t address) {
- uint8_t buff[2];
- buff[0] = address;
- HAL_I2C_Master_Transmit(&hi2c1, EEPROM_CHIP_ADDR << 1, buff, 1, 100);
- HAL_I2C_Master_Receive(&hi2c1, EEPROM_CHIP_ADDR << 1 | 1, &buff[1], 1, 100);
- return buff[1];
- }
-
- void EEPROM_Write(uint8_t address, uint8_t data) {
- uint8_t buff[2];
- uint8_t stored_data;
- buff[0] = address;
- buff[1] = data;
-
- stored_data = EEPROM_Read(address);
-
- if (stored_data != data)
- HAL_I2C_Master_Transmit(&hi2c1, EEPROM_CHIP_ADDR << 1, buff, 2, 100);
- }
-
- void EEPROM_2byte_Write(uint8_t addr, uint16_t data) {
- EEPROM_Write(addr, data & 0xff);
- HAL_Delay(5);
- EEPROM_Write(addr + 1, data >> 8);
- }
-
- uint16_t EEPROM_2byte_Read(uint8_t address) {
- uint16_t data = 0;
- data = EEPROM_Read(address + 1) << 8;
- HAL_Delay(5);
- data |= EEPROM_Read(address);
-
- return data;
- }
- */
-
 void m24c64_page_read(uint8_t address, uint8_t page, uint8_t *data) {
 	uint8_t buff[2] = { 0 };
 	uint16_t MemAddress = page << PADDRPOSITION;
